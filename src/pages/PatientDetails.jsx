d0b27d679369785ce4fa4b4a1dfa8026b7ddf6fe
Siddhi <siddhichordia21@gmail.com>
2026-06-26 14:51:29 +0530

    fix: correct case in generatePatientPdf import

--- src/pages/PatientDetails.jsx
+++ src/pages/PatientDetails.jsx
@@ -1,4 +1,4 @@
-import generatePatientPDF from "../utils/generatePatientPDF";
+import generatePatientPDF from "../utils/generatePatientPdf";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
